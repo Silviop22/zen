c9f4529a80d45086040689a0ecba6bcd97fe94f1
silvio.progonati <silvio.progonati@lhind.dlh.de>
2025-04-30 20:22:55 +0200

    Add proxy & integrate all components

---
+++ handler/connectionHandler.go
@@ -0,0 +1,72 @@
+package handler
+
+import (
+	"io"
+	"net"
+	"sync"
+	"zen/balancer"
+	"zen/utils/logger"
+)
+
+type ConnectionHandler struct {
+	balancer balancer.LoadBalancer
+}
+
+func NewConnectionHandler(balancer balancer.LoadBalancer) *ConnectionHandler {
+	return &ConnectionHandler{
+		balancer: balancer,
+	}
+}
+
+func (ch *ConnectionHandler) HandleConnection(clientConnection net.Conn) {
+	address := clientConnection.RemoteAddr().String()
+	logger.Info("New connection from {}", address)
+
+	backend, err := ch.balancer.Next()
+	if err != nil {
+		logger.Error("Failed to retrieve next available backend: {}", err)
+		clientConnection.Write([]byte("ERROR: Could not select backend server\n"))
+		clientConnection.Close()
+		return
+	}
+
+	backendConnection, err := backend.ConnectionPool.Get()
+	if err != nil {
+		logger.Error("Failed to get backend connection: {}", err)
+		clientConnection.Write([]byte("ERROR: Could not connect to backend server\n"))
+		clientConnection.Close()
+		return
+	}
+
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(2)
+
+	var clientToBackendErr, backendToClientErr error
+
+	go copy(backendConnection, clientConnection, &waitGroup, backendToClientErr)
+	go copy(clientConnection, backendConnection, &waitGroup, clientToBackendErr)
+
+	waitGroup.Wait()
+
+	//TODO: Mark backends as !alive
+	if clientToBackendErr != nil && clientToBackendErr != io.EOF {
+		logger.Error("Error copying client to backend for {}: {}", address, clientToBackendErr)
+	}
+	if backendToClientErr != nil && backendToClientErr != io.EOF {
+		logger.Error("Error copying backend to client for {}: {}", address, backendToClientErr)
+	}
+
+	logger.Info("Closing connection from {}", address)
+	backendConnection.Close()
+	clientConnection.Close()
+}
+
+func copy(source net.Conn, target net.Conn, waitGroup *sync.WaitGroup, connectionError error) {
+	defer waitGroup.Done()
+	_, connectionError = io.Copy(source, target)
+
+	if tcpConnection, ok := source.(*net.TCPConn); ok {
+		tcpConnection.CloseWrite()
+	}
+	logger.Debug("Data copy completed from {} to {}", source.RemoteAddr(), target.RemoteAddr())
+}
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"net"
+	"os"
+	"zen/backend"
+	"zen/balancer"
+	"zen/config"
+	"zen/handler"
+	"zen/utils/logger"
+)
+
+func init() {
+	level := logger.LevelInfo
+	if os.Getenv("DEBUG") == "1" {
+		level = logger.LevelDebug
+	}
+
+	logger.SetLevel(level)
+}
+
+func main() {
+	var configPath string
+	flag.StringVar(&configPath, "config", "config.yaml", "Path to the configuration file")
+	flag.Parse()
+
+	if configPath == "" {
+		configPath = "config.yaml"
+	}
+
+	var cfg config.Config
+	err := config.ParseConfig(&cfg, configPath)
+	if err != nil {
+		logger.Fatal("Failed to parse configuration file:", err)
+		os.Exit(1)
+	}
+
+	logger.Info("Starting server...")
+	ln, err := net.Listen("tcp", ":"+cfg.Server.Port)
+	if err != nil {
+		logger.Fatal("Failed to start server on port {}:", cfg.Server.Port, err)
+		cleanUp()
+		os.Exit(1)
+	}
+
+	backendPool := getBackendPool(&cfg)
+	backendPool.GetAliveBackends()
+	loadBalancer := balancer.NewRoundRobin(backendPool)
+
+	proxy := handler.NewConnectionHandler(loadBalancer)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			logger.Error("Failed to accept connection:", err)
+			continue
+		}
+
+		proxy.HandleConnection(conn)
+	}
+}
+
+func cleanUp() {
+	logger.Info("Shutting down server...")
+	//TODO: Perform cleanup
+	logger.Info("Server shut down successfully.")
+}
+
+func getBackendPool(cfg *config.Config) *backend.Pool {
+	logger.Info("Getting list of upstream servers...")
+	backendPool := backend.NewBackendPool(cfg.Upstream)
+	if backendPool == nil {
+		logger.Fatal("Failed to create backend pool")
+		cleanUp()
+		os.Exit(1)
+	}
+
+	logger.Info("Backend pool created with {} backends", len(backendPool.GetAliveBackends()))
+	return backendPool
+}
