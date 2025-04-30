33af9b11e0a40c131d3fd65ef4b956d3332dd94f
silvio.progonati <silvio.progonati@lhind.dlh.de>
2025-04-30 19:33:18 +0200

    Setup Logging

---
+++ utils/logger/logger.go
@@ -0,0 +1,78 @@
+package logger
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sync"
+)
+
+// Log levels
+const (
+	LevelDebug = iota
+	LevelInfo
+	LevelWarn
+	LevelError
+	LevelFatal
+)
+
+var (
+	mu       sync.Mutex
+	level    = LevelDebug // default
+	debugLog = log.New(os.Stdout, "DEBUG: ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+	infoLog  = log.New(os.Stdout, "INFO:  ", log.LstdFlags|log.Lmicroseconds)
+	warnLog  = log.New(os.Stdout, "WARN:  ", log.LstdFlags|log.Lmicroseconds)
+	errorLog = log.New(os.Stderr, "ERROR: ", log.LstdFlags|log.Lmicroseconds)
+	fatalLog = log.New(os.Stderr, "FATAL: ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+)
+
+func SetOutput(w io.Writer) {
+	mu.Lock()
+	defer mu.Unlock()
+	debugLog.SetOutput(w)
+	infoLog.SetOutput(w)
+	warnLog.SetOutput(w)
+	errorLog.SetOutput(w)
+	fatalLog.SetOutput(w)
+}
+
+func SetLevel(l int) {
+	mu.Lock()
+	defer mu.Unlock()
+	level = l
+}
+
+func Debug(v ...any) {
+	if level <= LevelDebug {
+		debugLog.Output(2, sprint(v...))
+	}
+}
+
+func Info(v ...any) {
+	if level <= LevelInfo {
+		infoLog.Output(2, sprint(v...))
+	}
+}
+
+func Warn(v ...any) {
+	if level <= LevelWarn {
+		warnLog.Output(2, sprint(v...))
+	}
+}
+
+func Error(v ...any) {
+	if level <= LevelError {
+		errorLog.Output(2, sprint(v...))
+	}
+}
+
+func Fatal(v ...any) {
+	if level <= LevelFatal {
+		fatalLog.Output(2, sprint(v...))
+	}
+}
+
+func sprint(v ...any) string {
+	return fmt.Sprintln(v...)
+}
